b0d52ed1887138f0666eaabfb6fbb64b304dd719
4OH4Loop <semperchuck@gmail.com>
2025-02-24 15:19:09 -0500

    Initial Commit

---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Carolyn Ballinger on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
